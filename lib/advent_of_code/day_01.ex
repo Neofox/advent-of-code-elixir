c4e4b92a03282c3a60542f83f6e8ebc6fe9bd1b5
Jerome Schaeffer <jer.schaeffer@gmail.com>
2024-12-01 18:03:37 +0900

    feat: improve parse_line function to handle empty input and refactor digit extraction logic

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -6,22 +6,37 @@ defmodule AdventOfCode.Day01 do
     |> Enum.sum()
   end
 
+  # defp parse_line_(line) do
+  #   digits = String.graphemes(line)
+  #   first = find_first_digit(digits)
+  #   last = find_first_digit(Enum.reverse(digits))
+
+  #   String.to_integer(first <> last)
+  # end
+
   defp parse_line(line) do
-    digits = String.graphemes(line)
-    first = find_first_digit(digits)
-    last = find_first_digit(Enum.reverse(digits))
+    digits =
+      Regex.scan(~r/\d+/, line)
+      |> List.flatten()
 
-    String.to_integer(first <> last)
+    # dbg(digits)
+    unless Enum.empty?(digits) do
+      first = List.first(digits)
+      last = List.last(digits)
+      String.to_integer(first <> last)
+    else
+      0
+    end
   end
 
-  defp find_first_digit(line) do
-    Enum.find(line, "0", fn char ->
-      case Integer.parse(char) do
-        {_, ""} -> true
-        _ -> false
-      end
-    end)
-  end
+  # defp find_first_digit(line) do
+  #   Enum.find(line, "0", fn char ->
+  #     case Integer.parse(char) do
+  #       {_, ""} -> true
+  #       _ -> false
+  #     end
+  #   end)
+  # end
 
   def part2(_args) do
   end
