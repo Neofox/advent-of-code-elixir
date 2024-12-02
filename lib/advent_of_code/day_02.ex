8ab88430be6ed9e6ca139665128693f414e93d4b
jerome <jerome@enhans.ai>
2024-12-02 17:47:00 +0900

    day 02

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,95 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def part1(unusual_data) do
+    make_report(unusual_data) |> Enum.count(&report_safe?/1)
   end
 
-  def part2(_args) do
+  def part2(unusual_data) do
+    make_report(unusual_data) |> Enum.count(&report_safe?(&1, true))
+  end
+
+  defp make_report(unusual_data) do
+    unusual_data
+    |> String.split("\n")
+    |> Enum.map(&parse_line/1)
+    |> Enum.reject(&(&1 === []))
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split()
+    |> Enum.reject(&(&1 === ""))
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp report_safe?(report, dampened \\ false) when is_list(report) do
+    case dampened do
+      true ->
+        all_levels_decreasing?(report, dampened) ||
+          all_levels_decreasing?(tl(report), dampened, true) ||
+          all_levels_increasing?(report, dampened) ||
+          all_levels_increasing?(tl(report), dampened, true)
+
+      false ->
+        all_levels_decreasing?(report) ||
+          all_levels_increasing?(report)
+    end
+  end
+
+  defp all_levels_decreasing?([head | tail], dampened \\ false, problem_dampened \\ false) do
+    if dampened do
+      do_all_levels_decreasing_dampened?(head, tail, problem_dampened)
+    else
+      do_all_levels_decreasing?(head, tail)
+    end
+  end
+
+  ### Non-dampened
+  defp do_all_levels_decreasing?(_val, []), do: true
+
+  defp do_all_levels_decreasing?(val, [head | tail]) do
+    case val - head >= 1 && val - head <= 3 do
+      true -> do_all_levels_decreasing?(head, tail)
+      false -> false
+    end
+  end
+
+  ### Dampened
+  defp do_all_levels_decreasing_dampened?(_val, [], _), do: true
+
+  defp do_all_levels_decreasing_dampened?(val, [head | tail], problem_dampened) do
+    case val - head >= 1 && val - head <= 3 do
+      true -> do_all_levels_decreasing_dampened?(head, tail, problem_dampened)
+      false when problem_dampened -> false
+      false -> do_all_levels_decreasing_dampened?(val, tail, true)
+    end
+  end
+
+  defp all_levels_increasing?([head | tail], dampened \\ false, problem_dampened \\ false) do
+    if dampened do
+      do_all_levels_increasing_dampened?(head, tail, problem_dampened)
+    else
+      do_all_levels_increasing?(head, tail)
+    end
+  end
+
+  ### Non-dampened
+  defp do_all_levels_increasing?(_val, []), do: true
+
+  defp do_all_levels_increasing?(val, [head | tail]) do
+    case head - val >= 1 && head - val <= 3 do
+      true -> do_all_levels_increasing?(head, tail)
+      false -> false
+    end
+  end
+
+  ### Dampened
+  defp do_all_levels_increasing_dampened?(_val, [], _), do: true
+
+  defp do_all_levels_increasing_dampened?(val, [head | tail], problem_dampened) do
+    case head - val >= 1 && head - val <= 3 do
+      true -> do_all_levels_increasing_dampened?(head, tail, problem_dampened)
+      false when problem_dampened -> false
+      false -> do_all_levels_increasing_dampened?(val, tail, true)
+    end
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,19 +3,32 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
     result = part1(input)
 
-    assert result
+    assert result === 2
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9"
     result = part2(input)
+    assert result === 4
 
-    assert result
+    input2 = "3 6 7 9 11 8
+84 86 85 82 81 79 76 75"
+
+    result2 = part2(input2)
+    assert result2 === 2
   end
 end
