0521fef1a04edf1e2694ca4dc4be4f0aefd5ee31
jerome <jerome@enhans.ai>
2024-12-03 16:23:44 +0900

    optimisation and refactor

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,22 +1,28 @@
 defmodule AdventOfCode.Day03 do
+  @mul_regex ~r/mul\((\d+),(\d+)\)/
+  @disable_regex ~r/don't\(\)(.*?)do\(\)/s
+
   def part1(instructions) do
-    instructions |> extract_params()
-  end
-
-  def part2(instructions) do
-    instructions |> remove_disabled_instructions() |> extract_params()
-  end
-
-  defp extract_params(instruction) do
-    ~r/mul\((\d+),(\d+)\)/
-    |> Regex.scan(instruction)
-    |> Stream.map(fn [_, x, y] -> {String.to_integer(x), String.to_integer(y)} end)
-    |> Stream.map(fn {x, y} -> x * y end)
+    instructions
+    |> extract_params()
     |> Enum.sum()
   end
 
+  def part2(instructions) do
+    instructions
+    |> remove_disabled_instructions()
+    |> extract_params()
+    |> Enum.sum()
+  end
+
+  defp extract_params(instruction) do
+    for [_, x, y] <- Regex.scan(@mul_regex, instruction) do
+      String.to_integer(x) * String.to_integer(y)
+    end
+  end
+
   defp remove_disabled_instructions(instruction) do
-    ~r/don't\(\)(.*?)do\(\)/s
+    @disable_regex
     |> Regex.replace(instruction, "")
   end
 end
