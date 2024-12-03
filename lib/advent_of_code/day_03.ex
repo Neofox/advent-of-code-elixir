7848fefb0e9b3ecb0103604d8346a33ac86fb1fd
jerome <jerome@enhans.ai>
2024-12-03 16:01:27 +0900

    day 03

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,24 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(instructions) do
+    instructions |> extract_params()
   end
 
-  def part2(_args) do
+  def part2(instructions) do
+    instructions |> remove_disabled_instructions() |> extract_params()
+  end
+
+  defp extract_params(instruction) do
+    regex = ~r/mul\((\d+),(\d+)\)/
+    all_params = Regex.scan(regex, instruction)
+
+    all_params
+    |> Stream.map(fn [_, x, y] -> {String.to_integer(x), String.to_integer(y)} end)
+    |> Stream.map(fn {x, y} -> x * y end)
+    |> Enum.sum()
+  end
+
+  defp remove_disabled_instructions(instruction) do
+    regex = ~r/don't\(\)(.*?)do\(\)/s
+    Regex.replace(regex, instruction, "")
   end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -3,19 +3,31 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    """
+
     result = part1(input)
 
-    assert result
+    assert result === 161
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+    """
+
     result = part2(input)
 
-    assert result
+    assert result === 48
+
+    custom_input = """
+    mul(2,3) don't()mul(4,5)do() mul(6,7) don't()mul(8,9)do()
+    """
+
+    result_custom = part2(custom_input)
+
+    assert result_custom === 48
   end
 end
