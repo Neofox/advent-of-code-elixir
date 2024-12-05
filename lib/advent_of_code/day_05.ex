f833ff79cb63dca238133dc39d74aad0b267d88c
jerome <jerome@enhans.ai>
2024-12-05 17:22:24 +0900

    day 5 cleanup

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -18,19 +18,14 @@ defmodule AdventOfCode.Day05 do
     |> Enum.sum()
   end
 
-  defp parse_input(safety_manual_input) do
-    [rules_section, pages_section] = String.split(safety_manual_input, "\n\n", trim: true)
-
-    ordering_rules = parse_ordering_rules(rules_section)
-    pages_to_produce = parse_pages_to_produce(pages_section)
-
-    {ordering_rules, pages_to_produce}
+  defp parse_input(input) do
+    [rules, pages] = String.split(input, "\n\n", trim: true)
+    {parse_ordering_rules(rules), parse_pages_to_produce(pages)}
   end
 
   defp parse_ordering_rules(safety_manual_input) do
     for line <- String.split(safety_manual_input, "\n", trim: true) do
-      [before_page, after_page] = String.split(line, "|")
-      {String.to_integer(before_page), String.to_integer(after_page)}
+      String.split(line, "|") |> Enum.map(&String.to_integer/1) |> List.to_tuple()
     end
     |> MapSet.new()
   end
@@ -41,28 +36,23 @@ defmodule AdventOfCode.Day05 do
     end
   end
 
-  defp safe_to_print?([], _pages_ordering_rules), do: true
-  defp safe_to_print?([_page], _pages_ordering_rules), do: true
+  defp safe_to_print?([], _ordering_rules), do: true
+  defp safe_to_print?([_page], _ordering_rules), do: true
 
-  defp safe_to_print?([page | following_pages], pages_ordering_rules) do
-    not ordering_rule_exists?(page, following_pages, pages_ordering_rules) and
-      safe_to_print?(following_pages, pages_ordering_rules)
+  defp safe_to_print?([page | following_pages], ordering_rules) do
+    not ordering_rule_exists?(page, following_pages, ordering_rules) and
+      safe_to_print?(following_pages, ordering_rules)
   end
 
-  defp ordering_rule_exists?(page, list_of_pages, pages_ordering_rules) do
-    Enum.any?(list_of_pages, fn page_before ->
-      MapSet.member?(pages_ordering_rules, {page_before, page})
-    end)
+  defp ordering_rule_exists?(page, list_of_pages, ordering_rules) do
+    Enum.any?(list_of_pages, &MapSet.member?(ordering_rules, {&1, page}))
   end
 
   defp get_page_in_middle(pages_to_produce) do
     Enum.at(pages_to_produce, div(length(pages_to_produce), 2))
   end
 
-  defp fix_pages_order(pages_to_produce, pages_ordering_rules) do
-    pages_to_produce
-    |> Enum.sort(fn page_before, page_after ->
-      {page_before, page_after} in pages_ordering_rules
-    end)
+  defp fix_pages_order(pages_to_produce, ordering_rules) do
+    Enum.sort(pages_to_produce, &MapSet.member?(ordering_rules, {&1, &2}))
   end
 end
