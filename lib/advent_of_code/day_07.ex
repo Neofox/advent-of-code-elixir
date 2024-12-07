456ea84d4801471fb2713fe632e114bab5b32c8b
Jerome Schaeffer <jer.schaeffer@gmail.com>
2024-12-07 15:06:02 +0900

    more cleanup

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,53 +1,42 @@
 defmodule AdventOfCode.Day07 do
   def part1(puzzle_input) do
-    String.split(puzzle_input, "\n", trim: true)
-    |> Enum.map(fn line ->
-      [result, numbers] = String.split(line, ": ")
-
-      {String.to_integer(result), String.split(numbers, " ") |> Enum.map(&String.to_integer/1)}
-    end)
-    |> Enum.filter(fn {result, numbers} -> find_sum_or_product(result, numbers) end)
-    |> Enum.map(fn {result, _} -> result end)
-    |> Enum.sum()
+    for {result, numbers} <- parse_input(puzzle_input),
+        result in do_operations(numbers, [:add, :multiply]),
+        reduce: 0 do
+      acc -> acc + result
+    end
   end
 
   def part2(puzzle_input) do
-    String.split(puzzle_input, "\n", trim: true)
+    for {result, numbers} <- parse_input(puzzle_input),
+        result in do_operations(numbers, [:add, :multiply, :concat]),
+        reduce: 0 do
+      acc -> acc + result
+    end
+  end
+
+  defp parse_input(puzzle_input) do
+    puzzle_input
+    |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
       [result, numbers] = String.split(line, ": ")
-
       {String.to_integer(result), String.split(numbers, " ") |> Enum.map(&String.to_integer/1)}
     end)
-    |> Enum.filter(fn {result, numbers} -> find_sum_or_product_or_concat(result, numbers) end)
-    |> Enum.map(fn {result, _} -> result end)
-    |> Enum.sum()
   end
 
-  defp find_sum_or_product(result, numbers) do
-    do_sum_or_product(numbers) |> Enum.member?(result)
+  defp do_operations([a, b], operations) do
+    Enum.map(operations, &apply_operation(&1, a, b))
   end
 
-  defp find_sum_or_product_or_concat(result, numbers) do
-    do_sum_or_product_or_concat(numbers) |> Enum.member?(result)
-  end
-
-  defp do_sum_or_product_or_concat([a, b]), do: [a + b, a * b, String.to_integer("#{a}#{b}")]
-
-  defp do_sum_or_product_or_concat([head | tail]) do
+  defp do_operations([head | tail], operations) do
     Enum.reduce(tail, [head], fn num, acc_results ->
       Enum.flat_map(acc_results, fn acc ->
-        [acc + num, acc * num, String.to_integer("#{acc}#{num}")]
+        Enum.map(operations, &apply_operation(&1, acc, num))
       end)
     end)
   end
 
-  defp do_sum_or_product([a, b]), do: [a + b, a * b]
-
-  defp do_sum_or_product([head | tail]) do
-    Enum.reduce(tail, [head], fn num, acc_results ->
-      Enum.flat_map(acc_results, fn acc ->
-        [acc + num, acc * num]
-      end)
-    end)
-  end
+  defp apply_operation(:add, a, b), do: a + b
+  defp apply_operation(:multiply, a, b), do: a * b
+  defp apply_operation(:concat, a, b), do: String.to_integer("#{a}#{b}")
 end
