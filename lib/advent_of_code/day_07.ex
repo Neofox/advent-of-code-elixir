514e94877a5094f4015105f959375edd05e50991
Jerome Schaeffer <jer.schaeffer@gmail.com>
2024-12-07 14:51:10 +0900

    day 07

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,53 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(puzzle_input) do
+    String.split(puzzle_input, "\n", trim: true)
+    |> Enum.map(fn line ->
+      [result, numbers] = String.split(line, ": ")
+
+      {String.to_integer(result), String.split(numbers, " ") |> Enum.map(&String.to_integer/1)}
+    end)
+    |> Enum.filter(fn {result, numbers} -> find_sum_or_product(result, numbers) end)
+    |> Enum.map(fn {result, _} -> result end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(puzzle_input) do
+    String.split(puzzle_input, "\n", trim: true)
+    |> Enum.map(fn line ->
+      [result, numbers] = String.split(line, ": ")
+
+      {String.to_integer(result), String.split(numbers, " ") |> Enum.map(&String.to_integer/1)}
+    end)
+    |> Enum.filter(fn {result, numbers} -> find_sum_or_product_or_concat(result, numbers) end)
+    |> Enum.map(fn {result, _} -> result end)
+    |> Enum.sum()
+  end
+
+  defp find_sum_or_product(result, numbers) do
+    do_sum_or_product(numbers) |> Enum.member?(result)
+  end
+
+  defp find_sum_or_product_or_concat(result, numbers) do
+    do_sum_or_product_or_concat(numbers) |> Enum.member?(result)
+  end
+
+  defp do_sum_or_product_or_concat([a, b]), do: [a + b, a * b, String.to_integer("#{a}#{b}")]
+
+  defp do_sum_or_product_or_concat([head | tail]) do
+    Enum.reduce(tail, [head], fn num, acc_results ->
+      Enum.flat_map(acc_results, fn acc ->
+        [acc + num, acc * num, String.to_integer("#{acc}#{num}")]
+      end)
+    end)
+  end
+
+  defp do_sum_or_product([a, b]), do: [a + b, a * b]
+
+  defp do_sum_or_product([head | tail]) do
+    Enum.reduce(tail, [head], fn num, acc_results ->
+      Enum.flat_map(acc_results, fn acc ->
+        [acc + num, acc * num]
+      end)
+    end)
   end
 end
